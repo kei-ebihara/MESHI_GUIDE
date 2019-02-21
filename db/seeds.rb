70ba4138c72d83fce1534344438e908d46db148d
海老原恵 <kei.ebihara.hikarituushin@gmail.com>
2019-02-21 17:30:35 +0900

    ジャンル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = %w(和定食・割烹 中華 アジア・エスニック 洋食 焼き肉・ホルモン ステーキ・肉料理 ラーメン 寿司・魚料理 そば・うどん カフェ・スイーツ バー・お酒 丼もの 鍋 居酒屋 バーガー・ファストフード ファミレス メイド喫茶  )
+categories = %w(和定食・割烹 中華 アジア・エスニック カレー 洋食 焼き肉・ホルモン ステーキ・肉料理 ラーメン 寿司・魚料理 そば・うどん カフェ・スイーツ バー・お酒 丼もの 鍋 居酒屋 バーガー・ファストフード ファミレス メイド喫茶  )
 
 categories.each do |category|
   Category.create(name: category)
